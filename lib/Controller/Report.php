e8a59ede340f8ac01c2d15649366465a1d4a5906
Andy Truong <andy@go1.com.au>
2014-02-20 13:37:12 +0700

    Fix #4: Render report using tabs.

--- lib/Controller/Report.php
+++ lib/Controller/Report.php
@@ -7,16 +7,25 @@ class Report {
       $reports['NodeTypes'] = 'Drupal\at_doc\Report\NodeTypes';
       $reports['InputFormats'] = 'Drupal\at_doc\Report\InputFormats';
       $reports['Views'] = 'Drupal\at_doc\Report\Views';
+
+      $output = array('reports' => array());
+
       foreach ($reports as $i => $report) {
         $report = new $report();
-        $output[] = array(
+        $output['reports'][$i] = array(
           '#prefix' => "<h3><a name='doc-{$i}' href='#doc-{$i}'>{$report->name}</a></h3>",
           $report->render()
         );
       }
 
-      $output[]['#markup'] = '<style>table td { vertical-align: top !important; }</style>';
-
-      return $output;
+      return array(
+        '#markup' => at_container('twig')->render('@at_doc/templates/report.html.twig', $output),
+        '#attached' => array(
+          'js' => array(
+            drupal_get_path('module', 'at_doc') . '/misc/js/at_report.js',
+          ),
+          'library' => array(array('system', 'ui.tabs', FALSE)),
+        ),
+      );
     }
 }
---
+++ misc/js/at_report.js
@@ -0,0 +1,8 @@
+(function($){
+
+Drupal.behaviors.atDocReport = {};
+Drupal.behaviors.atDocReport.attach = function(context, settings) {
+    $('.at-doc-reports').tabs();
+};
+
+})(jQuery);
