7fbc72870d2547bc8ca1f5db7bb3189a19ce8018
Andy Truong <andy@go1.com.au>
2014-02-20 10:20:54 +0700

    Tweak Views report

--- lib/Report/Views.php
+++ lib/Report/Views.php
@@ -9,24 +9,31 @@ class Views extends BaseReport {
    */
   public function process() {
     foreach (views_get_enabled_views() as $id => $view) {
-      $c1  = $view->export_module ? $view->export_module : 'N/A';
+      $c1  = $view->export_module ? $view->export_module : $this->iconError();
       $c2  = "<strong>{$view->human_name}</strong> ({$view->name})";
       $c2 .= _filter_autop($view->description);
       $c3  = $view->tag;
-      $c4  = array();
+      $links    = array();
+      $displays = array();
 
       foreach ($view->display as $display) {
         if ($display->display_plugin === 'page') {
           $link = url($display->display_options['path']);
-          $c4[] = l($link, $link);
+          $links[] = $link;
+          $displays[] = '<strong>'. $display->display_title .'</strong>' . ' ('. $display->display_plugin .')';
         }
       }
 
-      $rows[] = array($c1, $c2, $c3, implode(',', $c4));
+      $rows[] = array(
+        $c1,
+        $c2 . (empty($links) ? '' : theme('item_list', array('items' => $links, 'title' => t('Paths')))),
+        $c3,
+        implode(', ', $displays)
+      );
     }
 
     return array(
-      'header' => array('Feature', 'View', 'Tag', 'Path'),
+      'header' => array('Feature', 'View', 'Tag', 'Displays'),
       'rows' => $rows,
     );
   }
