996e297d905f7a6126d604369d8e65fc10e1ed3c
Tien Vo Xuan <tien@go1.com.au>
2014-03-10 14:59:59 +0700

    Don't print disabled modules.

--- lib/Report/Modules.php
+++ lib/Report/Modules.php
@@ -156,9 +156,11 @@ class Modules extends BaseReport
                 elseif ($all_modules[$requires]->status) {
                     $list[$requires] = t('@module (<span class="admin-enabled">enabled</span>)', array('@module' => $requires_name));
                 }
-                else {
-                    $list[$requires] = t('@module (<span class="admin-disabled">disabled</span>)', array('@module' => $requires_name));
-                }
+                // Don't print disabled modules.
+                // https://github.com/atdrupal/at_doc/issues/28
+//                else {
+//                    $list[$requires] = t('@module (<span class="admin-disabled">disabled</span>)', array('@module' => $requires_name));
+//                }
             }
         }
 
@@ -193,9 +195,11 @@ class Modules extends BaseReport
                 if ($all_modules[$required_by]->status == 1 && $status == 1) {
                     $list[] = t('@module (<span class="admin-enabled">enabled</span>)', array('@module' => $all_modules[$required_by]->info['name']));
                 }
-                else {
-                    $list[] = t('@module (<span class="admin-disabled">disabled</span>)', array('@module' => $all_modules[$required_by]->info['name']));
-                }
+                // Don't print disabled modules.
+                // https://github.com/atdrupal/at_doc/issues/28
+//                else {
+//                    $list[] = t('@module (<span class="admin-disabled">disabled</span>)', array('@module' => $all_modules[$required_by]->info['name']));
+//                }
             }
         }
 
