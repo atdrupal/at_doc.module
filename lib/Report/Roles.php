6922234dac96bf380c18b2f70a69219fbcfc8d1d
Tien Vo Xuan <tien@go1.com.au>
2014-03-11 10:57:00 +0700

    Report permissions by features.

--- lib/Report/Roles.php
+++ lib/Report/Roles.php
@@ -6,17 +6,56 @@ class Roles extends BaseReport {
 
   public function process() {
     foreach (user_role_permissions(user_roles()) as $role_id => $permissions) {
-      $permissions = theme(
-        'item_list',
-        array(
-          'items' => array_keys($permissions),
-          'attributes' => array('style' => '-webkit-column-count: 5; -moz-column-count: 5;'),
-        )
-      );
+      // Report permissions with feature.
+      // https://github.com/atdrupal/at_doc/issues/33
+      $exported_permissions = array();
+      $unexported_permissions = array();
+      $conflict_permissions = array();
+
+      $features_map = features_get_component_map('user_permission');
+      foreach ($permissions as $key => $value) {
+
+        if (isset($features_map[$key])) {
+          if (count($features_map[$key]) > 1) {
+            // Conflict.
+            $features = implode(', ', $features_map[$key]);
+            $conflict_permissions[] = $key . " ($features)";
+          }
+          else {
+            $feature = reset($features_map[$key]);
+            $exported_permissions[] = $key . " ($feature)";
+          }
+        }
+        else {
+          $unexported_permissions[] = $key;
+        }
+      }
+
+      $permissions_list = '<div class="permissions-list">';
+      if (!empty($exported_permissions)) {
+        $permissions_list .= theme('item_list', array(
+              'items' => $exported_permissions,
+            ));
+      }
+
+      if (!empty($unexported_permissions)) {
+        $permissions_list .=  $this->iconError() . " <strong>No feature</strong>: "
+          . theme('item_list', array(
+              'items' => $unexported_permissions,
+            ));
+      }
+
+      if (!empty($conflict_permissions)) {
+        $permissions_list .=  $this->iconError() . " <strong>Conflict</strong>: "
+          . theme('item_list', array(
+              'items' => $conflict_permissions,
+            ));
+      }
+      $permissions_list .= '</div>';
 
       $user_role_name = user_role_load($role_id)->name;
 
-      $rows[] = array($this->findRoleFeature($user_role_name), $user_role_name, $permissions);
+      $rows[] = array($this->findRoleFeature($user_role_name), $user_role_name, $permissions_list);
     }
 
     return array(
