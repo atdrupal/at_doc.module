fc2c4b8b94ced3cc10fa37939099683b9ad4a1ed
Tien Vo Xuan <tien@go1.com.au>
2014-03-19 11:32:16 +0700

    Support empty messages in views report

--- lib/Report/Views.php
+++ lib/Report/Views.php
@@ -8,19 +8,41 @@ class Views extends BaseReport {
    * @TODO: Col > Access
    */
   public function process() {
+    $rows = array();
+
     foreach (views_get_enabled_views() as $id => $view) {
-      $c1  = $view->export_module ? $view->export_module : $this->iconError();
+      $c1  = isset($view->export_module) ? $view->export_module : $this->iconError() . ' unknown';
       $c2  = "<strong>{$view->human_name}</strong> ({$view->name})";
       $c2 .= _filter_autop($view->description);
       $c3  = $view->tag;
+      $c4 = '';
       $links    = array();
       $displays = array();
 
       foreach ($view->display as $display) {
+        // Only process display page.
         if ($display->display_plugin === 'page') {
           $link = url($display->display_options['path']);
           $links[] = $link;
           $displays[] = '<strong>'. $display->display_title .'</strong>' . ' ('. $display->display_plugin .')';
+
+          // Empty behaviours.
+          if (isset($display->display_options['empty'])) {
+            // Overrided empty behaviours option.
+            $empty_behaviours = $display->display_options['empty'];
+          }
+          else {
+            $empty_behaviours = $view->display['default']->display_options['empty'];
+          }
+
+          // List readable empty messages.
+          $empty_messages = array();
+          foreach ($empty_behaviours as $key => $behaviour) {
+            if (in_array($behaviour['field'], array('area_text_custom', 'area')) && !empty($behaviour['content'])) {
+              $empty_messages[] = $behaviour['content'];
+            }
+          }
+          $c4 .= theme('item_list', array('items' => $empty_messages));
         }
       }
 
@@ -28,13 +50,16 @@ class Views extends BaseReport {
         $c1,
         $c2 . (empty($links) ? '' : theme('item_list', array('items' => $links, 'title' => t('Paths')))),
         $c3,
-        !empty($displays) ? implode(', ', $displays) : '<em>No display</em>'
+        !empty($displays) ? implode(', ', $displays) : '<em>No display</em>',
+        $c4
       );
     }
 
     return array(
-      'header' => array('Feature', 'View', 'Tag', 'Displays'),
-      'rows' => $rows,
+      '#theme' => 'table',
+      '#header' => array('Feature', 'View', 'Tag', 'Displays', 'Empty Message'),
+      '#rows' => $rows,
+      '#empty' => t('No enabled views'),
     );
   }
 }
